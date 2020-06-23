7609a7bc80f2e9b8591b53728a37618e45cae442
melisgb <gloriamelissa1@hotmail.com>
2020-06-23 12:47:08 +0100

    First layouts ready

--- app/src/main/java/com/example/instasocialwsapp/LoginActivity.kt
+++ app/src/main/java/com/example/instasocialwsapp/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.example.instasocialwsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        btnLogin.setOnClickListener{
+
+        }
+
+        btnGotoRegister.setOnClickListener{
+            val intent = Intent(applicationContext, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
